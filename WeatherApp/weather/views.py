157d87368a538875073b29f38ef11e64deee5c49
DanaKlimova <danonklimova@gmail.com>
2019-07-21 22:41:21 +0300

    city can be chosen

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
 import requests
+from .forms import CityForm
+from .models import City
+
 
 def index(request):
     appid = '42cc7376ff8fe1eaf19bb800bba4e8ba'
-    city = 'London'
+
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-    response = requests.get(url.format(city)).json()  #get data in dictionary format
+    form = CityForm() # нужно для отчистки формочки
 
-    city_info = {
-    'city': city,
-    'temp': response['main']['temp'],
-    'icon': response['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+    all_cities = []
 
-    context = {'info': city_info}
+    for city in cities:
+        response = requests.get(url.format(city.name)).json()  #get data in dictionary format
+        city_info = {
+        'city': city.name,
+        'temp': response['main']['temp'],
+        'icon': response['weather'][0]['icon']
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
     return render(request, 'weather/index.html', context)
